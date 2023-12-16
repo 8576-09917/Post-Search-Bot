6d36b77db624d2f0ac98b74d36727a6d7a22914d
8576-09917 <132528322+8576-09917@users.noreply.github.com>
2023-12-16 08:45:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
-API_ID       = 11970745
-API_HASH     = "095d152ad4d8cc7cf912bb142cbceed0"
+API_ID       = 21943287
+API_HASH     = "43b7ef733f4cf37770e737956fa3cec5"
 BOT_TOKEN    = "6012346500:AAGMVt_Y5e9UKbIiFm_uueP8GEmY4TMMYTU"
 SESSION      = "BQFO0_cASvhNziw7NrW2-w4sWGJ4F-Ub_jIA5PH7LTUjKyhIzbgpdFBOWLr7p2IBtDffZadZ8pe_nxGnRkug_KQY5jyAFrGAdrM0IJFeget1QnHD8H0Cpnv6hTe06IOyP79TA09bMcCkVwgRCAnukBY8HEa8hs9t9LTATd8LjQ4rGQDS7eV6iQviDkvLAslseM0BqtUotTDJdgg1-3Zfw_DeVeE9qiYJzkVFgZyV_cJX28yZzeL4uJVGaCj7DAscEHanWBEaozqSX9GDM9bJT-DrS10H2TaFGQFeEBB1_c7fJujGGyIbBmIv3M3ZtuW1tWalNDAAFyVsMnayFIOodod7ejbTpgAAAABMEn1lAA "
 DATABASE_URI = "mongodb+srv://Shazambot:password1234@cluster0.frux28j.mongodb.net/?retryWrites=true&w=majority"
